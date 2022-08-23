edc4f7bec620429ae2f96b80653baf7cadb2e293
user <alena_zar@mail.ru>
2022-08-23 11:17:26 +0300

    new test and assembly crash

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -13,8 +13,14 @@ public class StatisticsServiceTest {
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
 
-        long actual = service.findMax(incomesInBillions);
+        long[] incomesInBillions2 = {0, 5};
+        long expected2 = 5;
 
+        long actual = service.findMax(incomesInBillions);
         assertEquals(expected, actual);
+
+        long actual2 = service.findMax(incomesInBillions2);
+        assertEquals(expected2, actual2);
     }
+
 }
